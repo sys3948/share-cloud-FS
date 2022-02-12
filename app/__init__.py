fe9f8880d2f7c99d9700f1412808eee39416882b
yun <sys394880@gmail.com>
2022-02-12 15:32:07 +0900

    commit 2-1 : cors policy domain 허용 시도

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
-    cors.init_app(app)
+    cors.init_app(app, resources={r'*': {'origins':'192.168.56.1'}})
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
