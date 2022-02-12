8fd8ac13a5a9e5ca2047466d48b34d631ed4cd79
yun <sys394880@gmail.com>
2022-02-12 15:23:33 +0900

    commit  : cors policy  발생으로 해결하기 위한 테스트 코드 작성

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from flask_cors import CORS
 from config import config
+
+
+cors = CORS()
+
 
 def create_app(config_name):
     app = Flask(__name__)
 
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+
+    cors.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
