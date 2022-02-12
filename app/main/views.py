6c92fe6f608c9820fb69fac9c3773a55cc8955f8
yun <sys394880@gmail.com>
2022-02-12 16:22:39 +0900

    commit 2-7

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,13 @@
-from flask import jsonify
+from flask import jsonify, Response, request
 
 from . import main
 
 @main.route('/upload', methods=['GET', 'POST'])
 def upload():
-    print('hello World')
-    json_data = {'msg' : 'Hello World'}
-    return jsonify(json_data)
+    res = Response()
+    if request.method == 'POST':
+        print('upload/ POST Method 요청함.')
+        res.headers.add("Access-Control-Allow-Origin", "*")
+        res.headers.add("Access-Control-Allow-Headers", "*")
+    return res
+
