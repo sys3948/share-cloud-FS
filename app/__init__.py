bfbf7e22e3316f9082f67d38574bad6fd3e2a5e9
yun <sys394880@gmail.com>
2022-02-12 16:44:26 +0900

    commit 2-9

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
-    cors.init_app(app, resources={r'*': {'origins':'*'}})
+    cors.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
