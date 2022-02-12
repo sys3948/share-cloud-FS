bd08be4c880ccad5ef86d9ac83e41066e52fcfc8
yun <sys394880@gmail.com>
2022-02-12 17:16:33 +0900

    commit 2-11

--- app/__init__.py
+++ app/__init__.py
@@ -3,16 +3,13 @@ from flask_cors import CORS
 from config import config
 
 
-cors = CORS()
-
-
 def create_app(config_name):
     app = Flask(__name__)
 
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
-    cors.init_app(app)
+    CORS(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
