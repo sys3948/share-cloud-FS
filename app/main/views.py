7d3a533b154aa910f1d04fbe6d4a463c2d708731
yun <sys394880@gmail.com>
2022-02-13 23:16:54 +0900

    commit 3-3 : post request 요청한 data 암호화 풀어서 출력하기.

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,6 @@
-from flask import jsonify, request
+from flask import jsonify, request, current_app
+import os
+import cryptocode
 
 from . import main
 
@@ -14,7 +16,9 @@ def upload():
 @main.route('/create_folder', methods=['POST'])
 def create_folder():
     print('Http 통신 성공!')
-    print(request)
     print(request.form)
+    print(request.form.get('id'))
+    print(cryptocode.decrypt(request.form.get('id'), current_app.config('DECODE_KEY')))
+    # os.mkdir('/SFS/' + request.form.get('id'))
     return 'hello world!'
 
